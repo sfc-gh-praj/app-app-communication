04afc4074ab5569932ab402fd9227662ee15000e
sfc-gh-praj <phani.raj@snowflake.com>
2025-04-17 11:24:14 +0530

    adding comments

--- app_app_comm/build-and-push.sh
+++ app_app_comm/build-and-push.sh
@@ -9,6 +9,7 @@ IMAGE_NAME="fastapi-app"
 DIR_NAME="./source_app"
 
 # make sure the target repository exists
+echo "Creating DB and objects..."
 snow sql -q "create database if not exists $DB_NAME"
 snow sql -q "create schema if not exists $DB_NAME.$SCHEMA_NAME"
 snow sql -q "create image repository if not exists $DB_NAME.$SCHEMA_NAME.$IMAGE_REPO_NAME"
@@ -17,6 +18,12 @@ IMAGE_REPO_URL=$(snow spcs image-repository url $IMAGE_REPO_NAME --database $DB_
 IMAGE_FQN="$IMAGE_REPO_URL/$IMAGE_NAME"
 
 # build and push the image (uses :latest implicitly)
+echo "Building Docker image..."
 docker buildx build --platform=linux/amd64 -t $IMAGE_FQN $DIR_NAME
+
+echo "Logging into Snowflake registry..."
 snow spcs image-registry login
+
+echo "Pushing image to Snowflake registry..."
 docker image push $IMAGE_FQN
+echo "Build and push completed successfully!"
